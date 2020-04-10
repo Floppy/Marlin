2a08dd2c38e67f0ad9195e33221eecee7eb125aa
James Smith <james@floppy.org.uk>
2020-04-10 22:30:10 +0100

    set neopixel type

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2182,7 +2182,7 @@
 // Support for Adafruit Neopixel LED driver
 #define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
-  #define NEOPIXEL_TYPE   NEO_GRBW // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
+  #define NEOPIXEL_TYPE   NEO_GRB   // For WS2812B ring
   #define NEOPIXEL_PIN    PC7       // LED driving pin
   //#define NEOPIXEL2_TYPE NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN    5
